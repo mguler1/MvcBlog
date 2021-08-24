6c787a95de85f2a89dbb7a4d7d73e68d5fbbe876
= <=>
2021-08-24 17:11:02 +0300

    Contact alanı tamamlandı

---
+++ Business/Concrete/ContactManager.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Concrete;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+  public  class ContactManager
+    {
+        Repository<Contact> contact = new Repository<Contact>();
+        public int  BlContactAdd(Contact x)
+        {
+            if (x.Mail=="" || x.Message=="" ||x.Name=="" ||x.Subject=="" ||x.SurName=="" ||x.Mail.Length<=10 || x.Subject.Length<=3)
+            {
+                return -1;
+            }
+            return contact.Insert(x);
+        }
+    }
+}
--- MvcBlog/Controllers/ContactController.cs
+++ MvcBlog/Controllers/ContactController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Concrete;
+using Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +11,19 @@ namespace MvcBlog.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        ContactManager cm = new ContactManager();
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendMessage(Contact x)
+        {
+            cm.BlContactAdd(x);
             return View();
         }
     }
