6a516800bf221421bc35fe66337b68333331cb8e
= <=>
2021-09-19 20:51:49 +0300

    Blog detailsta resimler dinamik hale getirildi

---
+++ DataAccess/Migrations/202109191341480_BlogImageAdd.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BlogImageAdd : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Blogs", "BlogImage2", c => c.String());
+            AddColumn("dbo.Blogs", "BlogImage3", c => c.String());
+            AddColumn("dbo.Blogs", "BlogImage4", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Blogs", "BlogImage4");
+            DropColumn("dbo.Blogs", "BlogImage3");
+            DropColumn("dbo.Blogs", "BlogImage2");
+        }
+    }
+}
